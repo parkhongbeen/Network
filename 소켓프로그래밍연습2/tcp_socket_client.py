3ba581b6fd9e509fb0b52d6eb6c77f50e78b1381
parkhongbeen <pack122@naver.com>
2020-03-10 01:18:13 +0900

    osi7계층정리

---
+++ 소켓프로그래밍연습2/tcp_socket_client.py
@@ -0,0 +1,11 @@
+import socket, time
+
+HOST = '127.0.0.1'
+PORT = 9999
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((HOST, PORT))
+data = 'Hello Data!'
+client_socket.sendall(data.encode())
+time.sleep(30)
+client_socket.close()
